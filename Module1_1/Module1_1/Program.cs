cc18507865032abd7da636b8da2404861a31ee48
tanja8408 <tanja84.08@gmail.com>
2019-10-09 22:22:22 +0300

    Task 1 completed.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 15;
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
