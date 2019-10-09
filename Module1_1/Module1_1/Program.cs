a9eef9c56f034b122ad240c44d74cc6c84c1a929
mrgod85 <56307691+mrgod85@users.noreply.github.com>
2019-10-09 16:29:50 +0300

    New project was created. Task 1 was completed.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 4;
+            a += b;
+            b = -(b - a);
+            a -= b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
