2a2bbe9ce37f5b80985ff5cdf6be7612e4fb935f
mr.god855 <mr.god855@gmail.com>
2019-10-16 10:21:25 +0300

    Module 2 task 1 was completed

---
+++ module2_1/module2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число компаний, работающих в стране:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите налог в процентах:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int a = 500;
+            int b = a * n * m / 100;
+            Console.Write("сумма налога = {0}", b);
+            Console.ReadKey();
+        }
+    }
+}
