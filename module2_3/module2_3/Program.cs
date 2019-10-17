90d9e2daadfb2ba88c0944be5645d7d10ab6a14d
mr.god855 <mr.god855@gmail.com>
2019-10-17 15:36:58 +0300

    Module 2 task 3 completed

---
+++ module2_3/module2_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double result1;
+            Console.WriteLine("введите первое число: a ");
+            string a = Console.ReadLine().Replace('.', ',').Replace(' ', 'k');
+            while (!Double.TryParse(a, out result1))
+            {
+                Console.WriteLine("Вводите именно число: a ");
+                a = Console.ReadLine().Replace('.', ',').Replace(' ', 'k');
+                continue;
+            }
+
+            Double result2;
+            Console.WriteLine("введите второе число: b");
+            string b = Console.ReadLine().Replace('.', ',').Replace(' ', 'k');
+            while (!Double.TryParse(b, out result2))
+            {
+                Console.WriteLine("Вводите именно число: b");
+                b = Console.ReadLine().Replace('.', ',').Replace(' ', 'k');
+                continue;
+            }
+            double x = result1;
+            result1 = result2;
+            result2 = x;
+            {
+                Console.WriteLine("Результат : x={0} и y={1}", result1, result2);
+            }
+            Console.Read();
+        }
+    }
+}
