ad8d284b063caca9d1553b58c11e97cbadce8c05
mr.god85 <mr.god855@gmail.com>
2019-10-31 23:18:30 +0300

    done

---
+++ module2_2/module2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваш возраст:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0 & n > 18)
+            {
+                Console.WriteLine("Поздравляем с 18-летием!");
+            }
+            else if (n % 2 != 0 & n < 18 & n > 13)
+            {
+                Console.WriteLine("Поздравляем, школьник!");
+            }
+            else Console.WriteLine("чо за нах");
+            Console.ReadKey();
+        }
+    }
+}
